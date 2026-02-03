3026165061fbcb682b17c2f0a8ea0c1957a8a33c
klaudio0707 <claudioroberto0707@outlook.com>
2026-02-03 17:55:38 -0300

    feat: criar UsuarioControler com endpoints de cadastro

--- import-control/src/main/java/com/claudio/importcontrol/controller/UsuarioController.java
+++ import-control/src/main/java/com/claudio/importcontrol/controller/UsuarioController.java
@@ -1,5 +1,30 @@
 package com.claudio.importcontrol.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import com.claudio.importcontrol.dto.UsuarioDTO;
+import com.claudio.importcontrol.entity.Usuario;
+import com.claudio.importcontrol.service.UsuarioService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    
+
+    @Autowired
+    private UsuarioService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario criar(@RequestBody UsuarioDTO dados) {
+        return service.criar(dados);
+    }
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
 }
