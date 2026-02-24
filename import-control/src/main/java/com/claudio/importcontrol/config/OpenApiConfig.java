2714bdefc4bbc0e6f0a24ceb0198010a12034814
klaudio0707 <claudioroberto0707@outlook.com>
2026-02-24 18:04:45 -0300

    fix(docs): configura injecao global de token JWT no Swagger OpenAPI

--- import-control/src/main/java/com/claudio/importcontrol/config/OpenApiConfig.java
+++ import-control/src/main/java/com/claudio/importcontrol/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.claudio.importcontrol.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement; // <-- Importação nova e crucial
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +15,21 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("Import Control API")
-                .version("1.0")
-                .description("Sistema de Controle de Importações e Rastreio de Cargas")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                // Define como o botão funciona
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                //Dados da API
+                .info(new Info()
+                        .title("Import Control API")
+                        .version("1.0")
+                        .description("Sistema de Controle de Importações e Rastreio de Cargas")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+
+                // A MÁGICA AQUI: Obriga o Swagger a usar a chave 'bearer-key' em todas as rotas
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 }
